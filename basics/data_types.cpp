45f709ae48768fdb9b4dfaad85b79c494a2444e9
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-24 05:29:53 +0000

    learn data type and their sizes

---
+++ basics/data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 20;
+    double pi = 3.14159;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "age = " << age << '\n';
+    cout << "pi = " << pi << '\n';
+    cout << "grade = " << grade << '\n';
+    cout << "isStudent = " << isStudent << '\n';
+
+    cout << endl;
+
+    cout << "sizeof(age) = " << sizeof(age) << '\n';
+    cout << "sizeof(pi) = " << sizeof(pi) << '\n';
+    cout << "sizeof(grade) = " << sizeof(grade) << '\n';
+    cout << "sizeof(isStudent) = " << sizeof(isStudent)<< '\n';
+    
+}
